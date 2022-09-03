a418217aaa5188f1f9ae957d0d159d03a7a0dd98
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-09-03 23:36:42 +0800

    wip: added register user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,29 @@
 // dependencies
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
 const User = require("../models/user.model");
 
 // routes
 router.post("/register", async (req, res) => {
-  const user = await new User({
-    username: "Jeremiah",
-    email: "jeremiahtaylor2021@gmail.com",
-    password: "password",
-  });
+  try {
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(req.body.password, salt);
 
-  await user.save();
-  res.send("ok");
+    // create user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashed,
+    });
+
+    // save user
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
